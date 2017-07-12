12489661f527a327b1bd1faf6696cbde8cf60500
Michael Morris <mic@cgp.io>
2017-07-12 01:21:30 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,4 +1,9 @@
-fname = ['Michael','Beth']
-lname = ['Morris','Bushen']
+print('@ ------ The Amazing Name List ------ @')
+fname = ['Michael','Beth','George']
+lname = ['Morris','Bushen','Lucas']
+print()
+print('Pick a number between 1 and ' + str(len(fname)))
 userselect = int(input())
+userselect = userselect - 1
 print(fname[userselect] + ' ' + lname[userselect])
+print('@ ------------- THE END ------------- @')
