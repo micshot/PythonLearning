4c984c162e6028d0833fddb2b0960c5e974d5e5b
Michael Morris <mic@cgp.io>
2017-07-12 01:23:17 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,7 +3,6 @@ fname = ['Michael','Beth','George']
 lname = ['Morris','Bushen','Lucas']
 print()
 print('Pick a number between 1 and ' + str(len(fname)))
-userselect = int(input())
-userselect = userselect - 1
+userselect = (int(input())) - 1
 print(fname[userselect] + ' ' + lname[userselect])
 print('@ ------------- THE END ------------- @')
